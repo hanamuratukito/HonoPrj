0e50f64660d95ea0e4fa294f20299c221e4e23c2
hanamura <keikennouta11@gmail.com>
2024-09-08 16:49:14 +0900

    update：tailwindがデプロイされない問題確認2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./static/**/*.{js,jsx,ts,tsx}",
+    "./**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
