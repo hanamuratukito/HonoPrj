ff525a2cb5d4e3c506eec608df8fa2cbdeff4a51
hanamura <keikennouta11@gmail.com>
2024-09-08 19:11:33 +0900

    update：tailwindの読み込み修正

--- src/index.tsx
+++ src/index.tsx
@@ -79,7 +79,7 @@ app.get("*", (c) => {
           <meta content="width=device-width, initial-scale=1" name="viewport" />
           {import.meta.env.PROD ? (
             <>
-              <link href="/src/styles.css" rel="stylesheet"></link>
+              <link href="/src/style.css" rel="stylesheet"></link>
               <script type="module" src="/src/client.js"></script>
             </>
           ) : (
