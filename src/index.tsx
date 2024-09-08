8caba7839ca456439f03e2ed09ad65fe2d16a37d
hanamura <keikennouta11@gmail.com>
2024-09-08 15:42:59 +0900

    update：本番用に修正

--- src/index.tsx
+++ src/index.tsx
@@ -16,11 +16,7 @@ app.get("*", (c) => {
         <head>
           <meta charSet="utf-8" />
           <meta content="width=device-width, initial-scale=1" name="viewport" />
-          {import.meta.env.PROD ? (
-            <script type="module" src="/static/client.js"></script>
-          ) : (
-            <script type="module" src="/src/client.tsx"></script>
-          )}
+          <script type="module" src="/src/client.tsx"></script>
         </head>
         <body>
           <div id="root"></div>
